410362733f3cdc5f23f44c8481ea456866dd1a31
jittapat <idea26746@gmail.com>
2024-08-09 08:27:07 +0700

    ส่งงาน Assigment 7 (pokemon 0)

---
+++ assignment07/pokemon/pokemon00.py
@@ -0,0 +1,15 @@
+# อ่าน file ด้วย aiofiles
+import aiofiles
+import asyncio
+import json
+
+pokemonapi_directory = './assignment07/pokemon/pokemonapi'
+pokemonmove_directory = './assignment07/pokemon/pokemonmove'
+
+async def main():
+    #read the content of the json file.
+    async with aiofiles.open(f'{pokemonapi_directory}/articuno.json',mode='r') as f:
+        contents = await f.read()
+    print(contents)
+
+asyncio.run(main())
